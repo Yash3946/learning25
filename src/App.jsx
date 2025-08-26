2c810c2292ab6441e468786076316abdc084c305
Yash3946 <patelyash8262@gmail.com>
2025-08-26 08:24:50 +0530

    all

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {ContentComponent} from "./components/ContentComponent"
+import HeaderComponent from './components/HeaderComponent'
+import { FooterComponent } from './components/FooterComponent'
+import { MapDemo1 } from './components/MapDemo1'
+import { MapDemo2 } from './components/MapDemo2'
+
+
+function App() {
+
+  
+
+
+  return (
+    <div>
+      <HeaderComponent></HeaderComponent>
+      {/* <MapDemo1></MapDemo1> */}
+      <MapDemo2></MapDemo2>
+      {/* <ContentComponent></ContentComponent>
+      <FooterComponent></FooterComponent> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/HeaderComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+//<link rel styl>
+import "../assets/css/header.css"
+
+export default function HeaderComponent() {
+  return (
+    <div className='header'>
+        <h1>HEADER COMPONENT</h1>
+    </div>
+  )
+}
